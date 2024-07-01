7d14aa6280ef0ef3a50702f6d73056b594fad7a9
Luigi Cesaro <luigicesaro2003@libero.it>
2024-07-01 15:17:24 +0200

    Fine popolamento DB

--- Database/test.sql
+++ Database/test.sql
@@ -1,7 +1,7 @@
 -- Questo file riporta le varie insert per testare il database
 
 -- Family
-INSERT INTO smu.Family(name) VALUES ('Family 1');
+INSERT INTO smu.Family(name) VALUES ('Famiglia Rossi');
 
 -- User
 INSERT INTO smu.User(name, surname, username, password, email, taxCode, birthDate, idFamily) VALUES ('Mario', 'Rossi', 'user1', 'password1', 'mariorossi7@example.com', 'RSSMRA85M01H501Z', '1985-03-01', 1);
@@ -16,6 +16,8 @@ INSERT INTO smu.PaymentMethod(balance, iban, typeMethod, taxCode) VALUES (2000, 
 INSERT INTO smu.PaymentMethod(balance, iban, typeMethod, taxCode) VALUES (2000, 'IT12A1234567890123456789013', 'bankAccount', 'BNCLRA91C12F205X');
 -- Denaro contante di Luigi Verdi
 INSERT INTO smu.PaymentMethod(balance, iban, typeMethod, taxCode) VALUES (1000, 'cash', 'VRDLGI74P22H223W');
+-- Conto bancario di Luigi Verdi
+INSERT INTO smu.PaymentMethod(balance, iban, typeMethod, taxCode) VALUES (2000, 'IT12A1234567890123456789014', 'bankAccount', 'VRDLGI74P22H223W');
 
 -- Card
 
@@ -27,8 +29,18 @@ INSERT INTO smu.Card(cardNumber, cvv, expirationDate, ibanCard, typeCard, idPaym
 -- Portfolio
 
 -- Portfolio di Mario Rossi
-INSERT INTO smu.Portfolio(name, description, idUser) VALUES ('Portfolio 1', 'Portafoglio di Mario Rossi', 1);
+INSERT INTO smu.Portfolio(name, description, idUser, idFamily) VALUES ('Portfolio spese personali', 'Spese giornaliere di Mario Rossi', 1, NULL);
 -- Portfolio di Luigi Verdi
-INSERT INTO smu.Portfolio(name, description, idUser) VALUES ('Portfolio 2', 'Portafoglio di Luigi Verdi', 3);
+INSERT INTO smu.Portfolio(name, description, idUser, idFamily) VALUES ('Portfolio personale', 'Portafoglio generale di Luigi Verdi', 3, NULL);
+-- Portafoglio Famiglia Rossi
+INSERT INTO smu.Portfolio(name, description, idUser, idFamily) VALUES ('Portfolio Rossi', 'Bollette e spese utenze Famiglia Rossi', NULL, 1);
 
--- TODO: Aggiungere altre classi
+-- Transaction
+
+-- Transazione in uscita di Mario Rossi
+INSERT INTO smu.Transaction(amount, description, dateTime, receiver, sender, typeTransaction, idPaymentMethod, idPortfolio) VALUES (200, 'Spesa per la camera', '2022-01-01', 'Hotel Napoli', NULL, 'expense', 1, 1);
+-- Transazione in uscita Famiglia Rossi
+INSERT INTO smu.Transaction(amount, description, dateTime, receiver, sender, typeTransaction, idPaymentMethod, idPortfolio) VALUES (1000, 'Gas', '2022-01-01', 'Società Energia', NULL, 'expense', 1, 3);
+-- Transazione in entrata per Luigi Verdi
+INSERT INTO smu.Transaction(amount, description, dateTime, receiver, sender, typeTransaction, idPaymentMethod, idPortfolio) VALUES (2000, 'Stipendio mensile', '2022-01-01', NULL, 'Società Energia', 'income', 4, 2);
+
