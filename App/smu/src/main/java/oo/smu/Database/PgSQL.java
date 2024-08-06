103cc147fe7fa999832b438207def238ed69cf94
n00w4 <luigicesaro2003@libero.it>
2024-08-06 12:23:40 +0200

    [OO] Gestione connessione database

---
+++ App/smu/src/main/java/oo/smu/Database/PgSQL.java
@@ -0,0 +1,35 @@
+package oo.smu.Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PgSQL {
+	private String driver = "org.postgresql.Driver";
+	private String url = "jdbc:postgresql://localhost:5432/posgres?currentSchema=smu";
+	private String user = "postgres";
+	private String password = "postgres";
+	private static Connection connection;
+	
+	private PgSQL() throws SQLException {
+		try {
+			Class.forName(driver);
+			connection = DriverManager.getConnection(url, user, password);
+		} catch (ClassNotFoundException e) { e.printStackTrace(); }
+	}
+	
+	/**
+	 * Apre una connessione e la ritorna,
+	 * mentre in caso di errori viene chiusa,
+	 * riaprendone una nuova.
+	 * @throws SQLException
+	 */
+	public static Connection getConnection() throws SQLException {
+		if (connection == null || connection.isClosed()) {
+			new PgSQL();
+		}
+		return connection;
+	}
+	
+	
+}
