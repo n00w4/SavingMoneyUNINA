abad7aac4b219d948a630e803326af7238ade396
Luigi Cesaro <luigicesaro2003@libero.it>
2024-08-23 01:50:30 +0200

    [OO] Fix action listeners LoginFrame

--- App/smu/src/main/java/oo/smu/GUI/LoginFrame.java
+++ App/smu/src/main/java/oo/smu/GUI/LoginFrame.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 public class LoginFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private MainController mainController;
-	private JTextField usernameTextField;
+	private JTextField usernameField;
 	private JPasswordField passwordField;
 
     public LoginFrame(MainController mainController) {
@@ -20,6 +20,20 @@ public class LoginFrame extends JFrame {
     	setResizable(false);
         this.mainController = mainController;
         initComponents();
+    }
+    
+    private void performLogin() {
+    	String username = usernameField.getText();
+        String password = new String(passwordField.getPassword());
+
+		try {
+			if (mainController.tryLogin(username, password)) {
+                dispose();
+                mainController.showDashboardFrame();
+            } else {
+                JOptionPane.showMessageDialog(LoginFrame.this, "Login fallito!");
+            }
+		} catch (SQLException e1) { e1.printStackTrace(); }
     }
     
     private void initComponents() {
@@ -49,16 +63,16 @@ public class LoginFrame extends JFrame {
         lblUsername.setForeground(new Color(245, 245, 245));
         panel.add(lblUsername);
         
-        usernameTextField = new JTextField();
-        sl_panel.putConstraint(SpringLayout.SOUTH, lblUsername, -6, SpringLayout.NORTH, usernameTextField);
-        sl_panel.putConstraint(SpringLayout.EAST, usernameTextField, -35, SpringLayout.EAST, panel);
-        panel.add(usernameTextField);
-        usernameTextField.setColumns(10);
+        usernameField = new JTextField();
+        sl_panel.putConstraint(SpringLayout.SOUTH, lblUsername, -6, SpringLayout.NORTH, usernameField);
+        sl_panel.putConstraint(SpringLayout.EAST, usernameField, -35, SpringLayout.EAST, panel);
+        panel.add(usernameField);
+        usernameField.setColumns(10);
         
         JLabel lblPassword = new JLabel("Password");
         sl_panel.putConstraint(SpringLayout.WEST, lblPassword, 88, SpringLayout.WEST, panel);
         sl_panel.putConstraint(SpringLayout.WEST, lblUsername, 0, SpringLayout.WEST, lblPassword);
-        sl_panel.putConstraint(SpringLayout.SOUTH, usernameTextField, -6, SpringLayout.NORTH, lblPassword);
+        sl_panel.putConstraint(SpringLayout.SOUTH, usernameField, -6, SpringLayout.NORTH, lblPassword);
         lblPassword.setFont(new Font("Noto Sans", Font.PLAIN, 14));
         lblPassword.setForeground(new Color(245, 245, 245));
         panel.add(lblPassword);
@@ -66,18 +80,18 @@ public class LoginFrame extends JFrame {
         passwordField = new JPasswordField();
         sl_panel.putConstraint(SpringLayout.WEST, passwordField, 30, SpringLayout.WEST, panel);
         sl_panel.putConstraint(SpringLayout.EAST, passwordField, -35, SpringLayout.EAST, panel);
-        sl_panel.putConstraint(SpringLayout.WEST, usernameTextField, 0, SpringLayout.WEST, passwordField);
+        sl_panel.putConstraint(SpringLayout.WEST, usernameField, 0, SpringLayout.WEST, passwordField);
         sl_panel.putConstraint(SpringLayout.NORTH, passwordField, 265, SpringLayout.NORTH, panel);
         sl_panel.putConstraint(SpringLayout.SOUTH, lblPassword, -6, SpringLayout.NORTH, passwordField);
         panel.add(passwordField);
         
-        JButton btnLogin = new JButton("Log In");
-        sl_panel.putConstraint(SpringLayout.NORTH, btnLogin, 29, SpringLayout.SOUTH, passwordField);
-        sl_panel.putConstraint(SpringLayout.WEST, btnLogin, 30, SpringLayout.WEST, panel);
-        sl_panel.putConstraint(SpringLayout.EAST, btnLogin, -35, SpringLayout.EAST, panel);
-        btnLogin.setFont(new Font("Noto Sans", Font.PLAIN, 14));
-        btnLogin.setBackground(new Color(245, 245, 245));
-        panel.add(btnLogin);
+        JButton loginButton = new JButton("Log In");
+        sl_panel.putConstraint(SpringLayout.NORTH, loginButton, 29, SpringLayout.SOUTH, passwordField);
+        sl_panel.putConstraint(SpringLayout.WEST, loginButton, 30, SpringLayout.WEST, panel);
+        sl_panel.putConstraint(SpringLayout.EAST, loginButton, -35, SpringLayout.EAST, panel);
+        loginButton.setFont(new Font("Noto Sans", Font.PLAIN, 14));
+        loginButton.setBackground(new Color(245, 245, 245));
+        panel.add(loginButton);
         
         JLabel lblSavingmoneyunina = new JLabel("SavingMoneyUNINA");
         sl_panel.putConstraint(SpringLayout.NORTH, lblSavingmoneyunina, 10, SpringLayout.NORTH, panel);
@@ -85,6 +99,20 @@ public class LoginFrame extends JFrame {
         lblSavingmoneyunina.setForeground(new Color(245, 245, 245));
         lblSavingmoneyunina.setFont(new Font("Noto Sans", Font.BOLD, 22));
         panel.add(lblSavingmoneyunina);
+        
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                performLogin();
+            }
+        });
+
+        passwordField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                performLogin();
+            }
+        });
      }
 }
 
