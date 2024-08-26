5bc2dfbc7021b1422b65ead7cf3a0125cea62b64
Luigi Cesaro <luigicesaro2003@libero.it>
2024-08-27 01:32:03 +0200

    [OO] addTransactionPanel definitivo

--- App/smu/src/main/java/oo/smu/GUI/DashboardFrame.java
+++ App/smu/src/main/java/oo/smu/GUI/DashboardFrame.java
@@ -18,16 +18,17 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.Insets;
 import javax.swing.JTextField;
-import javax.swing.JSpinner;
 import javax.swing.JCheckBox;
 
 public class DashboardFrame extends JFrame {
     private static final long serialVersionUID = 1L;
     private MainController mainController;
     private JPanel cardPanel;
-    private JTextField textField;
-    private JTextField textField_1;
-    private JTextField textField_2;
+    private JTextField amountTextField;
+    private JTextField descriptionTextField;
+    private JTextField dateTextField;
+    private JTextField timeTextField;
+    private JTextField portfolioTextField;
 
     public DashboardFrame(MainController mainController) {
         this.mainController = mainController;
@@ -136,218 +137,210 @@ public class DashboardFrame extends JFrame {
     }
     
     private JPanel createAddTransactionPanel() {
-    	JPanel addTransactionPanel = new JPanel();
+        JPanel addTransactionPanel = new JPanel();
         addTransactionPanel.setBackground(new Color(23, 171, 96));
 
         GridBagLayout gbl_addTransactionPanel = new GridBagLayout();
-        gbl_addTransactionPanel.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-        gbl_addTransactionPanel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-        gbl_addTransactionPanel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-        gbl_addTransactionPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+        gbl_addTransactionPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+        gbl_addTransactionPanel.columnWeights = new double[]{1.0, 0.1, 0.0, 0.1, 1.0};
+        gbl_addTransactionPanel.rowHeights = new int[] {30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30};
         addTransactionPanel.setLayout(gbl_addTransactionPanel);
 
-        JLabel lblInserisciLaSomma = new JLabel("Inserisci la somma della transazione");
+        // Inserisci la somma
+        JLabel lblInserisciLaSomma = new JLabel("Inserisci la somma");
         lblInserisciLaSomma.setForeground(new Color(245, 245, 245));
         lblInserisciLaSomma.setFont(new Font("Noto Sans", Font.PLAIN, 14));
         GridBagConstraints gbc_lblInserisciLaSomma = new GridBagConstraints();
-        gbc_lblInserisciLaSomma.gridwidth = 8;
-        gbc_lblInserisciLaSomma.insets = new Insets(0, 0, 5, 5);
-        gbc_lblInserisciLaSomma.gridx = 9;
-        gbc_lblInserisciLaSomma.gridy = 1;
+        gbc_lblInserisciLaSomma.insets = new Insets(10, 0, 5, 5);
+        gbc_lblInserisciLaSomma.gridx = 2;
+        gbc_lblInserisciLaSomma.gridy = 0;
         addTransactionPanel.add(lblInserisciLaSomma, gbc_lblInserisciLaSomma);
-        
-        JLabel lblEuro = new JLabel("€");
-        lblEuro.setForeground(new Color(245, 245, 245));
-        GridBagConstraints gbc_lblEuro = new GridBagConstraints();
-        gbc_lblEuro.insets = new Insets(0, 0, 5, 5);
-        gbc_lblEuro.gridx = 9;
-        gbc_lblEuro.gridy = 2;
-        addTransactionPanel.add(lblEuro, gbc_lblEuro);
 
-        textField = new JTextField();
-        textField.setFont(new Font("Noto Sans", Font.PLAIN, 12));
-        GridBagConstraints gbc_textField = new GridBagConstraints();
-        gbc_textField.gridwidth = 7;
-        gbc_textField.insets = new Insets(0, 0, 5, 5);
-        gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-        gbc_textField.gridx = 10;
-        gbc_textField.gridy = 2;
-        addTransactionPanel.add(textField, gbc_textField);
-        textField.setColumns(10);
-                
-        JLabel lblInserisciLaDescrizione = new JLabel("Inserisci la descrizione (max 255 caratteri)");
+        // Simbolo Euro
+        JLabel lblNewLabel = new JLabel("€");
+        lblNewLabel.setForeground(new Color(245, 245, 245));
+        GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+        gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+        gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
+        gbc_lblNewLabel.gridx = 1;
+        gbc_lblNewLabel.gridy = 1;
+        addTransactionPanel.add(lblNewLabel, gbc_lblNewLabel);
+
+        // TextField per la somma
+        amountTextField = new JTextField();
+        amountTextField.setFont(new Font("Noto Sans", Font.PLAIN, 12));
+        GridBagConstraints gbc_amountTextField = new GridBagConstraints();
+        gbc_amountTextField.insets = new Insets(0, 0, 5, 5);
+        gbc_amountTextField.fill = GridBagConstraints.HORIZONTAL;
+        gbc_amountTextField.gridx = 2;
+        gbc_amountTextField.gridy = 1;
+        gbc_amountTextField.weightx = 0.1;
+        addTransactionPanel.add(amountTextField, gbc_amountTextField);
+        amountTextField.setColumns(10);
+
+        // Inserisci la descrizione
+        JLabel lblInserisciLaDescrizione = new JLabel("Inserisci la descrizione");
         lblInserisciLaDescrizione.setFont(new Font("Noto Sans", Font.PLAIN, 14));
         lblInserisciLaDescrizione.setForeground(new Color(245, 245, 245));
         GridBagConstraints gbc_lblInserisciLaDescrizione = new GridBagConstraints();
-        gbc_lblInserisciLaDescrizione.gridwidth = 10;
-        gbc_lblInserisciLaDescrizione.insets = new Insets(0, 0, 5, 5);
-        gbc_lblInserisciLaDescrizione.gridx = 9;
-        gbc_lblInserisciLaDescrizione.gridy = 3;
+        gbc_lblInserisciLaDescrizione.insets = new Insets(10, 0, 5, 5);
+        gbc_lblInserisciLaDescrizione.gridx = 2;
+        gbc_lblInserisciLaDescrizione.gridy = 2;
         addTransactionPanel.add(lblInserisciLaDescrizione, gbc_lblInserisciLaDescrizione);
-                
-        textField_1 = new JTextField();
-        GridBagConstraints gbc_textField_1 = new GridBagConstraints();
-        gbc_textField_1.gridwidth = 10;
-        gbc_textField_1.insets = new Insets(0, 0, 5, 5);
-        gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
-        gbc_textField_1.gridx = 9;
-        gbc_textField_1.gridy = 4;
-        addTransactionPanel.add(textField_1, gbc_textField_1);
-        textField_1.setColumns(10);
-                
+
+        // TextField per la descrizione
+        descriptionTextField = new JTextField();
+        descriptionTextField.setFont(new Font("Noto Sans", Font.PLAIN, 12));
+        GridBagConstraints gbc_descriptionTextField = new GridBagConstraints();
+        gbc_descriptionTextField.insets = new Insets(0, 0, 5, 5);
+        gbc_descriptionTextField.fill = GridBagConstraints.HORIZONTAL;
+        gbc_descriptionTextField.gridx = 2;
+        gbc_descriptionTextField.gridy = 3;
+        gbc_descriptionTextField.weightx = 0.1; 
+        addTransactionPanel.add(descriptionTextField, gbc_descriptionTextField);
+        descriptionTextField.setColumns(10);
+
+        // Inserisci la data e l'ora
         JLabel lblInserisciLaData = new JLabel("Inserisci la data e l'ora");
         lblInserisciLaData.setForeground(new Color(245, 245, 245));
         lblInserisciLaData.setFont(new Font("Noto Sans", Font.PLAIN, 14));
         GridBagConstraints gbc_lblInserisciLaData = new GridBagConstraints();
-        gbc_lblInserisciLaData.gridwidth = 8;
-        gbc_lblInserisciLaData.insets = new Insets(0, 0, 5, 5);
-        gbc_lblInserisciLaData.gridx = 10;
-        gbc_lblInserisciLaData.gridy = 5;
+        gbc_lblInserisciLaData.insets = new Insets(10, 0, 5, 5);
+        gbc_lblInserisciLaData.gridx = 2;
+        gbc_lblInserisciLaData.gridy = 4;
         addTransactionPanel.add(lblInserisciLaData, gbc_lblInserisciLaData);
-                
+
+        // Label Data
         JLabel lblData = new JLabel("Data:");
-        lblData.setForeground(new Color(245, 245, 245));
         lblData.setFont(new Font("Noto Sans", Font.BOLD, 12));
+        lblData.setForeground(new Color(245, 245, 245));
         GridBagConstraints gbc_lblData = new GridBagConstraints();
         gbc_lblData.insets = new Insets(0, 0, 5, 5);
-        gbc_lblData.gridx = 11;
-        gbc_lblData.gridy = 6;
+        gbc_lblData.anchor = GridBagConstraints.EAST;
+        gbc_lblData.gridx = 1;
+        gbc_lblData.gridy = 5;
         addTransactionPanel.add(lblData, gbc_lblData);
-                
-        JSpinner daySpinner = new JSpinner();
-        daySpinner.setFont(new Font("Noto Sans", Font.BOLD, 12));
-        daySpinner.setBackground(new Color(245, 245, 245));
-        GridBagConstraints gbc_daySpinner = new GridBagConstraints();
-        gbc_daySpinner.insets = new Insets(0, 0, 5, 5);
-        gbc_daySpinner.gridx = 12;
-        gbc_daySpinner.gridy = 6;
-        addTransactionPanel.add(daySpinner, gbc_daySpinner);
-        
-        JSpinner monthSpinner = new JSpinner();
-        monthSpinner.setFont(new Font("Noto Sans", Font.BOLD, 12));
-        monthSpinner.setBackground(new Color(245, 245, 245));
-        GridBagConstraints gbc_monthSpinner = new GridBagConstraints();
-        gbc_monthSpinner.insets = new Insets(0, 0, 5, 5);
-        gbc_monthSpinner.gridx = 13;
-        gbc_monthSpinner.gridy = 6;
-        addTransactionPanel.add(monthSpinner, gbc_monthSpinner);
-        
-        JSpinner yearSpinner = new JSpinner();
-        yearSpinner.setFont(new Font("Noto Sans", Font.BOLD, 12));
-        yearSpinner.setBackground(new Color(245, 245, 245));
-        GridBagConstraints gbc_yearSpinner = new GridBagConstraints();
-        gbc_yearSpinner.insets = new Insets(0, 0, 5, 5);
-        gbc_yearSpinner.gridx = 14;
-        gbc_yearSpinner.gridy = 6;
-        addTransactionPanel.add(yearSpinner, gbc_yearSpinner);
 
+        // TextField per la data
+        dateTextField = new JTextField();
+        dateTextField.setFont(new Font("Noto Sans", Font.PLAIN, 12));
+        GridBagConstraints gbc_dateTextField = new GridBagConstraints();
+        gbc_dateTextField.insets = new Insets(0, 0, 5, 5);
+        gbc_dateTextField.fill = GridBagConstraints.HORIZONTAL;
+        gbc_dateTextField.gridx = 2;
+        gbc_dateTextField.gridy = 5;
+        gbc_dateTextField.weightx = 0.1; 
+        addTransactionPanel.add(dateTextField, gbc_dateTextField);
+        dateTextField.setColumns(10);
+
+        // Label Ora
         JLabel lblOra = new JLabel("Ora:");
         lblOra.setForeground(new Color(245, 245, 245));
         lblOra.setFont(new Font("Noto Sans", Font.BOLD, 12));
         GridBagConstraints gbc_lblOra = new GridBagConstraints();
         gbc_lblOra.insets = new Insets(0, 0, 5, 5);
-        gbc_lblOra.gridx = 11;
-        gbc_lblOra.gridy = 7;
+        gbc_lblOra.anchor = GridBagConstraints.EAST;
+        gbc_lblOra.gridx = 1;
+        gbc_lblOra.gridy = 6;
         addTransactionPanel.add(lblOra, gbc_lblOra);
 
-        JSpinner hourSpinner = new JSpinner();
-        hourSpinner.setFont(new Font("Noto Sans", Font.BOLD, 12));
-        hourSpinner.setBackground(new Color(245, 245, 245));
-        GridBagConstraints gbc_hourSpinner = new GridBagConstraints();
-        gbc_hourSpinner.insets = new Insets(0, 0, 5, 5);
-        gbc_hourSpinner.gridx = 12;
-        gbc_hourSpinner.gridy = 7;
-        addTransactionPanel.add(hourSpinner, gbc_hourSpinner);
-        
-        JSpinner minuteSpinner = new JSpinner();
-        minuteSpinner.setFont(new Font("Noto Sans", Font.BOLD, 12));
-        minuteSpinner.setBackground(new Color(245, 245, 245));
-        GridBagConstraints gbc_minuteSpinner = new GridBagConstraints();
-        gbc_minuteSpinner.insets = new Insets(0, 0, 5, 5);
-        gbc_minuteSpinner.gridx = 13;
-        gbc_minuteSpinner.gridy = 7;
-        addTransactionPanel.add(minuteSpinner, gbc_minuteSpinner);
-        
-        JSpinner secondSpinner = new JSpinner();
-        secondSpinner.setFont(new Font("Noto Sans", Font.BOLD, 12));
-        secondSpinner.setBackground(new Color(245, 245, 245));
-        GridBagConstraints gbc_secondSpinner = new GridBagConstraints();
-        gbc_secondSpinner.insets = new Insets(0, 0, 5, 5);
-        gbc_secondSpinner.gridx = 14;
-        gbc_secondSpinner.gridy = 7;
-        addTransactionPanel.add(secondSpinner, gbc_secondSpinner);
+        // TextField per l'ora
+        timeTextField = new JTextField();
+        timeTextField.setFont(new Font("Noto Sans", Font.PLAIN, 12));
+        GridBagConstraints gbc_timeTextField = new GridBagConstraints();
+        gbc_timeTextField.insets = new Insets(0, 0, 5, 5);
+        gbc_timeTextField.fill = GridBagConstraints.HORIZONTAL;
+        gbc_timeTextField.gridx = 2;
+        gbc_timeTextField.gridy = 6;
+        gbc_timeTextField.weightx = 0.1; 
+        addTransactionPanel.add(timeTextField, gbc_timeTextField);
+        timeTextField.setColumns(10);
 
+        // Specifica il tipo di transazione
         JLabel lblSpecificaIlTipo = new JLabel("Specifica il tipo di transazione");
         lblSpecificaIlTipo.setForeground(new Color(245, 245, 245));
         lblSpecificaIlTipo.setFont(new Font("Noto Sans", Font.PLAIN, 14));
         GridBagConstraints gbc_lblSpecificaIlTipo = new GridBagConstraints();
-        gbc_lblSpecificaIlTipo.gridwidth = 8;
-        gbc_lblSpecificaIlTipo.insets = new Insets(0, 0, 5, 5);
-        gbc_lblSpecificaIlTipo.gridx = 9;
-        gbc_lblSpecificaIlTipo.gridy = 8;
+        gbc_lblSpecificaIlTipo.insets = new Insets(10, 0, 5, 5);
+        gbc_lblSpecificaIlTipo.gridx = 2;
+        gbc_lblSpecificaIlTipo.gridy = 7;
         addTransactionPanel.add(lblSpecificaIlTipo, gbc_lblSpecificaIlTipo);
-
-        JCheckBox chckbxEntrata = new JCheckBox("Entrata");
-        chckbxEntrata.setFont(new Font("Noto Sans", Font.PLAIN, 12));
-        chckbxEntrata.setBackground(new Color(245, 245, 245));
-        GridBagConstraints gbc_chckbxEntrata = new GridBagConstraints();
-        gbc_chckbxEntrata.gridwidth = 2;
-        gbc_chckbxEntrata.insets = new Insets(0, 0, 5, 5);
-        gbc_chckbxEntrata.gridx = 10;
-        gbc_chckbxEntrata.gridy = 9;
-        addTransactionPanel.add(chckbxEntrata, gbc_chckbxEntrata);
         
-        JCheckBox chckbxUscita = new JCheckBox("Uscita");
-        chckbxUscita.setFont(new Font("Noto Sans", Font.PLAIN, 12));
-        GridBagConstraints gbc_chckbxUscita = new GridBagConstraints();
-        gbc_chckbxUscita.gridwidth = 2;
-        gbc_chckbxUscita.insets = new Insets(0, 0, 5, 5);
-        gbc_chckbxUscita.gridx = 14;
-        gbc_chckbxUscita.gridy = 9;
-        addTransactionPanel.add(chckbxUscita, gbc_chckbxUscita);
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(23, 171, 96));
+        GridBagConstraints gbc_panel = new GridBagConstraints();
+        gbc_panel.insets = new Insets(0, 0, 5, 5);
+        gbc_panel.fill = GridBagConstraints.BOTH;
+        gbc_panel.gridx = 2;
+        gbc_panel.gridy = 8;
+        addTransactionPanel.add(panel, gbc_panel);
+        
+        // CheckBox Entrata
+        JCheckBox chckbxEntrata = new JCheckBox("Entrata");
+        panel.add(chckbxEntrata);
+        chckbxEntrata.setFont(new Font("Noto Sans", Font.PLAIN, 14));
+        chckbxEntrata.setBackground(new Color(23, 171, 96));
+        chckbxEntrata.setForeground(new Color(245, 245, 245));
 
-        JLabel lblInserisciNomeDel = new JLabel("Inserisci nome del Portfolio di destinazione");
+        // CheckBox Uscita
+        JCheckBox chckbxUscita = new JCheckBox("Uscita");
+        panel.add(chckbxUscita);
+        chckbxUscita.setFont(new Font("Noto Sans", Font.PLAIN, 14));
+        chckbxUscita.setBackground(new Color(23, 171, 96));
+        chckbxUscita.setForeground(new Color(245, 245, 245));
+        
+        JPanel panel_1 = new JPanel();
+        panel_1.setBackground(new Color(23, 171, 96));
+        GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+        gbc_panel_1.insets = new Insets(0, 0, 5, 0);
+        gbc_panel_1.gridx = 4;
+        gbc_panel_1.gridy = 8;
+        addTransactionPanel.add(panel_1, gbc_panel_1);
+
+        // Inserisci il nome del Portfolio
+        JLabel lblInserisciNomeDel = new JLabel("Inserisci il nome del Portfolio di destinazione");
         lblInserisciNomeDel.setForeground(new Color(245, 245, 245));
         lblInserisciNomeDel.setFont(new Font("Noto Sans", Font.PLAIN, 14));
         GridBagConstraints gbc_lblInserisciNomeDel = new GridBagConstraints();
-        gbc_lblInserisciNomeDel.gridwidth = 10;
-        gbc_lblInserisciNomeDel.insets = new Insets(0, 0, 5, 5);
-        gbc_lblInserisciNomeDel.gridx = 9;
-        gbc_lblInserisciNomeDel.gridy = 10;
+        gbc_lblInserisciNomeDel.insets = new Insets(10, 0, 5, 5);
+        gbc_lblInserisciNomeDel.gridx = 2;
+        gbc_lblInserisciNomeDel.gridy = 9;
         addTransactionPanel.add(lblInserisciNomeDel, gbc_lblInserisciNomeDel);
 
-        textField_2 = new JTextField();
-        GridBagConstraints gbc_textField_2 = new GridBagConstraints();
-        gbc_textField_2.gridwidth = 10;
-        gbc_textField_2.insets = new Insets(0, 0, 5, 5);
-        gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
-        gbc_textField_2.gridx = 9;
-        gbc_textField_2.gridy = 11;
-        addTransactionPanel.add(textField_2, gbc_textField_2);
-        textField_2.setColumns(10);
+        // TextField per il Portfolio
+        portfolioTextField = new JTextField();
+        GridBagConstraints gbc_portfolioTextField = new GridBagConstraints();
+        gbc_portfolioTextField.insets = new Insets(0, 0, 5, 0);
+        gbc_portfolioTextField.fill = GridBagConstraints.HORIZONTAL;
+        gbc_portfolioTextField.gridx = 2;
+        gbc_portfolioTextField.gridy = 10;
+        gbc_portfolioTextField.weightx = 0.1;
+        addTransactionPanel.add(portfolioTextField, gbc_portfolioTextField);
+        portfolioTextField.setColumns(10);
 
+        // Bottone Salva
         JButton btnSalva = new JButton("Salva");
+        btnSalva.setFont(new Font("Noto Sans", Font.PLAIN, 14));
         btnSalva.setBackground(new Color(245, 245, 245));
-        btnSalva.setFont(new Font("Noto Sans", Font.PLAIN, 12));
         GridBagConstraints gbc_btnSalva = new GridBagConstraints();
-        gbc_btnSalva.gridwidth = 3;
-        gbc_btnSalva.insets = new Insets(0, 0, 5, 5);
-        gbc_btnSalva.gridx = 12;
-        gbc_btnSalva.gridy = 14;
+        gbc_btnSalva.insets = new Insets(20, 0, 5, 5);
+        gbc_btnSalva.gridx = 2;
+        gbc_btnSalva.gridy = 11;
         addTransactionPanel.add(btnSalva, gbc_btnSalva);
 
-        JButton btnTornaAllaDashboard = new JButton("Torna alla dashboard");
-        btnTornaAllaDashboard.setBackground(new Color(245, 245, 245));
-        btnTornaAllaDashboard.setFont(new Font("Noto Sans", Font.PLAIN, 12));
-        GridBagConstraints gbc_btnTornaAllaDashboard = new GridBagConstraints();
-        gbc_btnTornaAllaDashboard.gridwidth = 5;
-        gbc_btnTornaAllaDashboard.insets = new Insets(0, 0, 0, 5);
-        gbc_btnTornaAllaDashboard.gridx = 11;
-        gbc_btnTornaAllaDashboard.gridy = 15;
-        addTransactionPanel.add(btnTornaAllaDashboard, gbc_btnTornaAllaDashboard);
-        
+        // Bottone Torna alla dashboard
+        JButton btnDashboard = new JButton("Torna alla dashboard");
+        btnDashboard.setBackground(new Color(245, 245, 245));
+        btnDashboard.setFont(new Font("Noto Sans", Font.PLAIN, 14));
+        GridBagConstraints gbc_btnDashboard = new GridBagConstraints();
+        gbc_btnDashboard.insets = new Insets(0, 0, 0, 5);
+        gbc_btnDashboard.gridx = 2;
+        gbc_btnDashboard.gridy = 12;
+        addTransactionPanel.add(btnDashboard, gbc_btnDashboard);
+
         return addTransactionPanel;
     }
+
 }
 
 
